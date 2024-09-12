e78ce84b7cfc74636382d73ffc3489bac5582f63
riadhasanmitul <95171391+riadhasanmitul@users.noreply.github.com>
2024-09-12 18:08:12 +0600

    Add files via upload

---
+++ test.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define fastio ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define ll long long
+#define YY cout<<"YES"<<endl;
+#define NN cout<<"NO"<<endl;
+#define sz s.size()
+#define pb push_back
+#define ss " "
+#define all v.begin(),v.end()
+#define endl "\n"
+const int N=1e5+10;
+    int test=1;
+void solve()
+{
+    cout<<test;
+}
+signed main()
+{
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+    fastio
+    cin>>test;
+    while (test--)
+    {
+        solve();
+    }
+    return 0;
+}
